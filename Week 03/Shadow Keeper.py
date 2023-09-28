72bb5554628600851114fad222261b04643d5848
Soheeui <145718267+Soheeui@users.noreply.github.com>
2023-09-28 14:23:06 -0400

    chaged

---
+++ Week 03/Shadow Keeper.py
@@ -0,0 +1,182 @@
+
+#ShadowKeepr
+
+def printGraphic(name):
+    if(name == "title"):
+        print('   ▄▄▄▄▄    ▄  █ ██   ██▄   ████▄   ▄ ▄       █  █▀ ▄███▄   ▄███▄   █ ▄▄  ▄███▄   █▄▄▄▄') 
+        print(' █     ▀▄ █   █ █ █  █  █  █   █  █   █      █▄█   █▀   ▀  █▀   ▀  █   █ █▀   ▀  █  ▄▀ ')
+        print('▄  ▀▀▀▀▄   ██▀▀█ █▄▄█ █   █ █   █ █ ▄   █     █▀▄   ██▄▄    ██▄▄    █▀▀▀  ██▄▄    █▀▀▌ ')
+        print('▀▄▄▄▄▀    █   █ █  █ █  █  ▀████ █  █  █     █  █  █▄   ▄▀ █▄   ▄▀ █     █▄   ▄▀ █  █  ')
+        print('            █     █ ███▀         █ █ █        █   ▀███▀   ▀███▀    █    ▀███▀     █    ')
+        print('          ▀     █                ▀ ▀        ▀                      ▀            ▀      ')
+        print('              ▀                                                                        ')
+
+    if(name == "one"):
+        print(' .   .        .        .        . .   .    .   .    .   .        . .        .         .   ')
+        print('    .   .    .   .    .   .          .        .   .    .        .     .    .   .    .   . ')
+        print('  .   .        .   .    .        . .   .    .   .    .   .        . .        .        .   ')
+        print('    .   .    .   .    .   . .        .        .        .        .     .    .   .    .   . ')
+        print('  . . . .    . . .      . . .    . . . .    . . .    . . . .    . . .      . .      . . . ')
+        print('  . . . .    . . . .  . . . .    . . . .    . . .    . . . .    . . . .  . . . .    . . . ')
+        print('  . . . .    . . . .  . . . .    . . . .  . . . . .  . . . .    . . . .  . . . .    . . . ')
+        print('  . . . . .. . . . .  . . . . .  . . . .  . . . .    . . . .  . . . . .  . . . . .. . . . ')
+
+    if(name == 'two'):
+        print(' .   .        .        .        . .   .    .   .    .   .        . .        .         .   ')
+        print('    .   .    .   .    .   .          .        .   .    .        .     .    .   .    .   . ')
+        print('  .   .        .   .    .        . .   .    .   .    .   .        . .        .        .   ')
+        print('    .   .    .   .    .   . .        .        .        .        .     .    .   .    .   . ')
+        print('  . . . .    . . .      . . .    . . . .    . . .    . . . .    . . .      . .      . . . ')
+        print('  . . . .    . . . .  . . . .    . . . .    . . .    . . . .    . . . .  . . . .    . . . ')
+        print('  . . . .    . . . .  . . . .    . . . .  . . . . .  . . . .    . . . .  . . . .    . . . ')
+        print('  . . . . .. . . . .  . . . . .  . . . .  . . . .    . . . .  . . . . .  . . . . .. . . . ')
+
+    if(name == 'three'):
+        print(' .    .   \'       \'        \'  .    \'\'  `    \'  ,,    .  -\' ..   ;). "\'  .I" i-`  ``')
+        print('     .       \'        \'  .    \'. .^    .  :,      .}\' \'\'   :{..,\'  .;^ +]` .`:.`n  v')
+        print('.       \'        \'  .    `. .`    .  ~,      .|. \'.   ~)..:\'  \';` _?\'  `:.^u} \'\'I^`')
+        print('   \'        .  .    `. .`    .  i^    . \'[. \'.   <_ .,\'  \'I` __\' .^: ^r- \'\'\^ \'"\'')
+        print('      ..  \'    `  .`    .  <^      \'-  `.   !> \',\'  \';. }?\' .`, :u? ^\'\'..jv:\'i"` (')
+        print('..  \'    \'  .`       +\'      `~  ^\'   !; `;\'  \':../}. .`^ ;v! "\'\'.`jc,`+,\'..l,<1;v')
+        print('\'    \'  \'`      .(\'      ^?  ^.  .~; `:.  \':..f[. .`^ -v> "\'`\' "nv^`>\'". !\\}\<"+v~')
+        print('`  `\'      .[\'      ,~  ^.  .~" `:.  `;\'./_  .`` +n: "\'`.^ru`^!"\'.l\\-\'f)l.,+[vj\\uuu')
+
+    if(name == 'four'):
+        print("▒▒▓▓██████████████▓▓▓▓▒▒░░░░░░░░░░░░░▒▒▒▓▓▓▓▓▒▒▒▓▓▓▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██")
+        print("▓▒▒▒░░░▒▒▓▓█████████▓▓▒▒░░░░░░░░░░░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒░▒▒▒▓▓▓▓▓")
+        print("████████▓▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓██████████████")
+        print("▒▒▓▓▓▓▓▓▓▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▓▓▓████████████████████████")
+    
+    if(name == 'five'):
+        print("██████████████████████████████████████████████████████████████████████████████████████████")
+        print("██████████████████████████████████████████████████████████████████████████████████████████")
+        print("▒▒▒▒▒▒▓███████████████████████████████████████████████████████████████████████████████████")
+        print("▓▓▓▓▓▒▒░░░▒▓███████████████████████████████████████▓▓▓▓███████████████████████████████████")
+        print("██████▓▓▓▒▒░░░▒▓████████████████████████▓▓▒▒░░░░░░░░░░░░░░░░▒▒▓▓▓█████████████████████████")
+        print("█████████▓▓▒▒▒░░░░▒▓████████████████▓▒░░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░▒▓▓█████▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+        print("██████████▓▓▒▒▒░░░░░░░▒▒▓█████████▒░░░░▒▒▒▒▒▒▓▓▓▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒░░░░░░░")
+        print("████████████▓▓▓▒▒░░░░░░░░░░░░▒▒░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█░")
+
+    if(name == 'six'):
+        print("██████████████████████████████████████████████████████████████████████████████████████████")
+        print("██████████████████████████████████████████████████████████████████████████████████████████")
+        print("▒▒▒▒▒▒▓███████████████████████████████████████████████████████████████████████████████████")
+        print("▓▓▓▓▓▒▒░░░▒▓███████████████████████████████████████▓▓▓▓███████████████████████████████████")
+        print("██████▓▓▓▒▒░░░▒▓████████████████████████▓▓▒▒░░░░░░░░░░░░░░░░▒▒▓▓▓█████████████████████████")
+        print("█████████▓▓▒▒▒░░░░▒▓████████████████▓▒░░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░▒▓▓█████▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+        print("██████████▓▓▒▒▒░░░░░░░▒▒▓█████████▒░░░░▒▒▒▒▒▒▓▓▓▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒░░░░░░░")
+        print("████████████▓▓▓▒▒░░░░░░░░░░░░▒▒░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█░")
+        print("▒▒▓▓██████████████▓▓▓▓▒▒░░░░░░░░░░░░░▒▒▒▓▓▓▓▓▒▒▒▓▓▓▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██")
+        print("▓▒▒▒░░░▒▒▓▓█████████▓▓▒▒░░░░░░░░░░░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒░▒▒▒▓▓▓▓▓")
+        print("████████▓▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓██████████████")
+        print("▒▒▓▓▓▓▓▓▓▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▓▓▓████████████████████████")
+
+player = { 
+    "name": "p1", 
+}
+
+
+def jeorney1():
+    print("Where do you live?")
+    print("options: [Not yet, NJ]") 
+
+    answer = input(">")
+
+    if answer == "Not yet": 
+        one()
+    elif answer == "NJ":
+        jeorney2()
+
+def jeorney2():
+    print("What number of bus do you should take to go School?")
+    print("options: [156, 156R]") 
+
+    answer = input(">")
+
+    if answer == "156":
+        two()
+    elif answer == "156R":
+        jeorney3()
+
+def jeorney3():
+    print("On the way to go home you saw homeless who is holding the door for people. What can you do for him?")
+    print("options: [Say Thanks, Nothig]") 
+
+    answer = input(">")
+
+    if answer == "Say Tahnks":
+        three()
+    elif answer == "Nothing":
+        jeorney4()
+
+def jeorney4():
+    print("In the mart you meet the man, Joe who is your neighborhood, he introudce by himself to you and he is curious about you because of you are asian. He say are you a japaness?")
+    print("options: [No, Run]") 
+
+    answer = input(">")
+
+    if answer == "No":
+        four()
+    elif answer == "Run":
+        jeorney5()
+
+def jeorney5():
+    print("You meet the stranger in the park. And he wants to talk with you, How long can you stay and talk with him?")
+    print("options: [under 8min, over 8min]") 
+
+    answer = input(">")
+
+    if answer == "under 8min":
+        five()
+    elif answer == "over 8min":
+        jeorney6()
+
+def jeorney6():
+    print("You meet the stranger in the park. And he wants to talk with you, How long can you stay and talk with him?")
+    print("options: [under 8min, over 8min]") 
+
+    answer = input(">")
+
+    if answer == "under 8min":
+        six()
+    elif answer == "over 8min":
+        five()
+    
+
+def one():
+    print("Your shadow has disappeared. Your existence fades away.")
+    printGraphic("one")
+
+def two():
+    print("Your shadow is dwindling. You'll soon be forgotten.")
+    printGraphic("two")
+
+def three():
+    print("Your shadow is running out. Your existence is fading fast.")
+    printGraphic("three")
+
+def four():
+    print("You've barely preserved your existence... but for how long?")
+    printGraphic("four")
+
+def five():
+    print("Your existence is slowly becoming known to people.")
+    printGraphic("five")
+
+def six():
+    print("There are times when people think of you.")
+    printGraphic("six")
+
+def introStory():
+    
+    print("Welcome to ShadowKeeper")
+    printGraphic("title")
+    player["name"] = input("Please enter your name >")
+    print(player["name"] + " Your existence is tied to your shadow")
+    print("When your shadow fades...")
+    print( player["name"] + ", your memory in the minds of friends and family will fades...")
+
+    jeorney1()
+
+introStory()
+
